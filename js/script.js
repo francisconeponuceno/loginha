f1db37b5912321ddb6230115458433b7f88597af
francisconeponuceno <120109535+francisconeponuceno@users.noreply.github.com>
2024-01-09 17:52:32 -0300

    ATUALIZACAO

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
-let pr_unitario = document.getElementById('p_unitario').value;
-let quantidade = Number(document.getElementById('qtd').value);
-let total = Number(document.getElementById('total').value);
 
+function alterarQtd(produto, acao){
+    let qtd = document.getElementById('qtd_' + produto)
+    let valor = document.getElementById('valor_' + produto)
+    let total = document.getElementById('total_' + produto)
 
-function texte(){
+    if (acao == '-' && qtd.innerHTML == 0){
+        alert('Atenção! A quantidade não pode ser menor que 0.')
+    }else{
+        acao == '+' ? qtd.innerHTML++ : qtd.innerHTML--
+        total.innerHTML = qtd.innerHTML * valor.innerHTML
+        soma()
+    }
     
-    alert(pr_unitario)
+}
+
+function soma(){
+    let soma = 0
+    for(let i = 1; i < 4; i++){
+        soma += Number(document.getElementById('total_' + i).innerHTML)
+    }
+    document.getElementById('subtotal').innerHTML = soma
 }
