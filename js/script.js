090a0ef0521f9f4167478ef228e3773f21e30bac
francisconeponuceno <120109535+francisconeponuceno@users.noreply.github.com>
2024-01-09 10:42:27 -0300

    atualizacao

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let pr_unitario = document.getElementById('p_unitario').value;
+let quantidade = Number(document.getElementById('qtd').value);
+let total = Number(document.getElementById('total').value);
+
+
+function texte(){
+    
+    alert(pr_unitario)
+}
